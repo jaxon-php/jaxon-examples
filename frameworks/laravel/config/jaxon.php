a8993c2a88bfd394dce682647c39544d61ebecdf
feuzeu <thierry.feuzeu@gmail.com>
2016-06-15 23:00:17 -0400

    Updated Laravel example configuration.

--- frameworks/laravel/config/jaxon.php
+++ frameworks/laravel/config/jaxon.php
@@ -27,7 +27,7 @@ return array(
         ),
         'js' => array(
             'lib' => array(
-                'uri' => '/jaxon/lib',
+                // 'uri' => '/jaxon/lib',
             ),
             'app' => array(
                 // 'uri' => '',
