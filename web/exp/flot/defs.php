dda9e211f05165179324a292c783b267e97954f5
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-05-18 03:38:47 +0200

    Fixes.

--- web/exp/flot/defs.php
+++ web/exp/flot/defs.php
@@ -16,11 +16,16 @@ class Flot extends \Jaxon\App\CallableClass
         // for($i = 0; $i < 10; $i++) $ticks[] = [$i, 'Pt' . $i];
         // $plot->xaxis()->points($ticks);
         $plot->xaxis()->expr(0, 16, 1, 'plots.xaxis.label');
-        // Add a graph to the plot
+
+        // Add a first graph to the plot
         $graph = $plot->graph(['lines' => ['show' => true], 'label' => 'Sqrt']);
         $graph->series()->expr(0, 14, 0.5, 'plots.sqrt.value', 'plots.sqrt.label');
+
+        // Add a second graph to the plot
         $graph = $plot->graph(['lines' => ['show' => true], 'points' => ['show' => true], 'label' => 'Graph 2']);
         $graph->series()->points([[0, 3, 'Pt 1'], [4, 8, 'Pt 2'], [8, 5, 'Pt 3'], [9, 13, 'Pt 4']]);
+
+        // Draw the graph
         $this->response->flot->draw($plot);
         // Return the response
         return $this->response;
